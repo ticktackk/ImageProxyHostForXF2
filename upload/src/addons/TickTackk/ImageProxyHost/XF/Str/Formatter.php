5475a222c9e020de2243dd486713b7d6732f0247
ticktackk <31729605+ticktackk@users.noreply.github.com>
2018-05-04 14:14:53 +0530

    PHPDoc stuff

--- upload/src/addons/TickTackk/ImageProxyHost/XF/Str/Formatter.php
+++ upload/src/addons/TickTackk/ImageProxyHost/XF/Str/Formatter.php
@@ -2,12 +2,23 @@
 
 namespace TickTackk\ImageProxyHost\XF\Str;
 
+/**
+ * Class Formatter
+ *
+ * @package TickTackk\ImageProxyHost
+ */
 class Formatter extends XFCP_Formatter
 {
+    /**
+     * @param $type
+     * @param $url
+     *
+     * @return null|string
+     */
     public function getProxiedUrlIfActive($type, $url)
     {
         $return = parent::getProxiedUrlIfActive($type, $url);
-        if ($type == 'image')
+        if ($type === 'image')
         {
             $options = \XF::app()->options();
             $proxyHost = $options->bfp_proxyhost;
@@ -15,7 +26,7 @@ class Formatter extends XFCP_Formatter
 
             $linkClassTarget = $this->getLinkClassTarget($url);
 
-            if ($linkClassTarget['type'] == 'external' && $ignoreSSLImages && substr($url, 0, 8) == "https://")
+            if ($linkClassTarget['type'] === 'external' && $ignoreSSLImages && substr($url, 0, 8) === 'https://')
             {
                 return $url;
             }
