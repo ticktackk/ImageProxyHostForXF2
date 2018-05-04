f3c2ce973cbc2b25fcb3db7f5a938bdeef229915
ticktackk <31729605+ticktackk@users.noreply.github.com>
2018-05-04 14:15:13 +0530

    Add missing extension_hint.php file (again?)

---
+++ upload/src/addons/TickTackk/ImageProxyHost/_output/extension_hint.php
@@ -0,0 +1,9 @@
+<?php
+
+// ################## THIS IS A GENERATED FILE ##################
+// DO NOT EDIT DIRECTLY. EDIT THE CLASS EXTENSIONS IN THE CONTROL PANEL.
+
+namespace TickTackk\ImageProxyHost\XF\Str
+{
+	class XFCP_Formatter extends \XF\Str\Formatter {}
+}
