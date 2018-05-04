068958e1f261ed9876542c41b03a3263b143b1b5
ticktackk <31729605+ticktackk@users.noreply.github.com>
2018-05-04 13:57:39 +0530

    Add .extension_hint.php

--- upload/src/addons/TickTackk/ImageProxyHost/_output/extension_hint.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// ################## THIS IS A GENERATED FILE ##################
-// DO NOT EDIT DIRECTLY. EDIT THE CLASS EXTENSIONS IN THE CONTROL PANEL.
-
-namespace TickTackk\ImageProxyHost\XF\Str
-{
-	class XFCP_Formatter extends \XF\Str\Formatter {}
-}
