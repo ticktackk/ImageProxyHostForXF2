654bc9cc4b21b4ab2c7faad2e30af9499b580afd
TickTackk <me@ticktackk.io>
2020-07-19 17:59:34 +0530

    closes #1

--- XF/Str/Formatter.php
+++ XF/Str/Formatter.php
@@ -2,41 +2,78 @@
 
 namespace TickTackk\ImageProxyHost\XF\Str;
 
-/**
- * Class Formatter
- *
- * @package TickTackk\ImageProxyHost
- */
 class Formatter extends XFCP_Formatter
 {
     /**
-     * @param $type
-     * @param $url
+     * @param string $type Type of the URL that is being passed. XenForo supports image or link
+     * @param string $url The URL that is being proxified.
      *
-     * @return null|string
+     * @return string
      */
     public function getProxiedUrlIfActive($type, $url)
     {
-        $return = parent::getProxiedUrlIfActive($type, $url);
-        if ($type === 'image')
+        $proxiedUrl = parent::getProxiedUrlIfActive($type, $url);
+
+        if (\XF::$versionId < 2010010) // XF 2.0 support
         {
-            $options = \XF::app()->options();
-            $proxyHost = $options->bfp_proxyhost;
-            $ignoreSSLImages = $options->bfp_ignoressl;
-
-            $linkClassTarget = $this->getLinkClassTarget($url);
-
-            if ($linkClassTarget['type'] === 'external' && $ignoreSSLImages && substr($url, 0, 8) === 'https://')
-            {
-                return $url;
-            }
-
-            if (!empty($proxyHost))
-            {
-                return rtrim($proxyHost, '/') . '/' . ltrim(strstr($return, 'proxy.php'), '/');
-            }
+            $proxiedUrl = $this->applyTckImageProxyHost($type, $url, $proxiedUrl);
         }
 
-        return $return;
+        return $proxiedUrl;
+    }
+
+    /**
+     * @param string $type Type of the URL that is being passed. XenForo supports image or link
+     * @param string $url The URL that is being proxified.
+     * @param array $options Additional options
+     *
+     * @return string|null Returns null if invalid/disabled proxy type is provided
+     */
+    public function getProxiedUrlIfActiveExtended($type, $url, array $options = [])
+    {
+        $proxiedUrl = parent::getProxiedUrlIfActiveExtended($type, $url, $options);
+
+        if ($proxiedUrl !== null)
+        {
+            $proxiedUrl = $this->applyTckImageProxyHost($type, $url, $proxiedUrl);
+        }
+
+        return $proxiedUrl;
+    }
+
+    /**
+     * @param string $type Type of the URL that is being passed. XenForo supports image or link
+     * @param string $originalUrl The URL that is being proxified.
+     * @param string $proxiedUrl The URL that has been proxified
+     *
+     * @return string The final url
+     */
+    protected function applyTckImageProxyHost($type, $originalUrl, $proxiedUrl)
+    {
+        $linkClassTarget = $this->getLinkClassTarget($originalUrl);
+
+        if ($type !== 'image' || $linkClassTarget['type'] !== 'external')
+        {
+            return $proxiedUrl;
+        }
+
+        $options = \XF::app()->options();
+        $proxyHost = $options->bfp_proxyhost;
+        $ignoreSSLImages = \boolval($options->bfp_ignoressl);
+
+        if ($ignoreSSLImages && \strtolower(\parse_url($originalUrl, \PHP_URL_SCHEME)) === 'https')
+        {
+            return $originalUrl;
+        }
+
+        if (empty($proxyHost))
+        {
+            return $proxiedUrl;
+        }
+
+        $proxyHost = \rtrim($proxyHost, '/');
+        $proxiedUrl = \ltrim($proxiedUrl, '/');
+
+        return "{$proxyHost}/{$proxiedUrl}";
     }
 }
